7ae8609d692ab3a4baca3ca4fb07afd1a6dc3aa9
Nekich <nekitkz165@gmail.com>
2026-05-11 18:51:13 +0300

    upd

--- static/scrypts/menuSort.js
+++ static/scrypts/menuSort.js
@@ -1,8 +1,9 @@
 const filterButtons = document.querySelectorAll('.products-filter button');
-const productItems = document.querySelectorAll('.products-list-item');
+
 
 filterButtons.forEach((button) => {
     button.addEventListener('click', () => {
+        const productItems = document.querySelectorAll('.products-list-item');
         const selectedCategory = button.dataset.category;
 
         filterButtons.forEach((btn) => btn.classList.remove('active'));
